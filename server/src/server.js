59625bbed75428c839371d3f45b9ef810d48d9d4
Oscar Ravelo <oscar91ravelo@gmail.com>
2024-11-05 17:41:07 +0100

    adding first endpoint (not working correctly)

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,15 @@
 const PORT = 8000;
 
+const http = require("http");
+
+const { loadPlanetsData } = require("./models/planets.model");
+
 const server = require("./app");
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+startServer();
